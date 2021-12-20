863a3c068667448d5fad3970e74a81304f29b9aa
SwathiKrishna27 <swathikrishna2627@gmail.com>
2021-12-21 00:26:53 +0530

    1st Project

---
+++ src/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Greet = () => {
+    return (
+        <div>
+            <h1><center> HELLO WORLD! </center></h1>
+        </div>
+    )
+}
+
+export default Greet;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Greet from './Greet';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Greet />
   </React.StrictMode>,
   document.getElementById('root')
 );
